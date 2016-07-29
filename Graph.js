c6ffc5c14abff0d250b240aa0ff5b2e197e221e7
Nils <nilsgawlik@gmx.de>
2016-07-29 16:49:11 +0200

    undo unnecessary test code

--- Graph.js
+++ Graph.js
@@ -47,10 +47,6 @@
   reached_limit() - returns true if the limit has been reached, otherwise false
 
  */
-  node = new Node(1);
-  node.position.x = 100;
-  node.position.y = 100;
-  node.data.title = "Title of the node";
 
 function Graph(options) {
   this.options = options || {};
